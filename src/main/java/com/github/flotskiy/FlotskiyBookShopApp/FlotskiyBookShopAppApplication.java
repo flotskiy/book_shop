4307b240a55d73c96030a514d996e95468c6168a
Alexander Flotskiy <yattvp@ya.ru>
2022-07-29 23:07:41 +0300

    initial

---
+++ src/main/java/com/github/flotskiy/FlotskiyBookShopApp/FlotskiyBookShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.flotskiy.FlotskiyBookShopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlotskiyBookShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlotskiyBookShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/flotskiy/FlotskiyBookShopApp/FlotskiyBookShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.flotskiy.FlotskiyBookShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlotskiyBookShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

791f7bb19cfd11268769a4f96593153b65042a7a
Alexander Flotskiy <yattvp@ya.ru>
2022-08-15 21:45:57 +0300

    H2 replaced by PostgreSQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,16 @@
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
 
 CREATE TABLE books(
-    id INT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     price VARCHAR(250) DEFAULT NULL,
     price_old VARCHAR(250) DEFAULT NULL,
     title VARCHAR(250) NOT NULL,
-    author_id INT NOT NULL
+    author_id SERIAL NOT NULL
 );
 
-DROP TABLE IF EXISTS authors;
-
 create table authors (
-     id INT PRIMARY KEY ,
+     id BIGSERIAL PRIMARY KEY ,
      first_name VARCHAR(50),
      last_name VARCHAR(50)
 );
